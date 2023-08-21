3303c4a9a1751b64bec9f9e0a0a98204bba5cb62
awdhesh-prasad <prasadawdheshp145@gmail.com>
2023-08-21 13:06:46 +0530

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const authMiddleware = require("../middlewares/authMiddlware");
+
+const {
+  registerController,
+  loginController,
+  forgotPasswordController,
+  authController,
+  getAllPropertiesController,
+  bookingHandleController,
+  getAllBookingsController,
+} = require("../controllers/userController");
+
+const router = express.Router();
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+
+router.post("/register", registerController);
+
+router.post("/login", loginController);
+
+router.post("/forgotpassword", forgotPasswordController);
+
+router.get('/getAllProperties', getAllPropertiesController)
+
+router.post("/getuserdata", authMiddleware, authController);
+
+router.post("/bookinghandle/:propertyid", authMiddleware, bookingHandleController);
+
+router.get('/getallbookings', authMiddleware, getAllBookingsController)
+
+module.exports = router;
